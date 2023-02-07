1c4ece64e71141aadde0f8b73793d10c8c7d7782
unknown <ebinaliyas5@gmail.com>
2023-02-07 18:40:18 +0530

    grid add main area

--- src/getCurrentLocation.js
+++ src/getCurrentLocation.js
@@ -1,30 +1,29 @@
-import { apiKey } from "./Apiid";
+import { apiKey } from './Apiid';
 
-
-let latitudex = 0
-let longitudex = 0
+let latitudex = 0;
+let longitudex = 0;
 
 const getLocation = document.getElementById('getLocation');
 
 function showPosition(position) {
   latitudex = position.coords.latitude;
   longitudex = position.coords.longitude;
-  displayToPage()
+  displayToPage();
 }
 
 export function locationX() {
-  navigator.geolocation.getCurrentPosition(showPosition)
+  navigator.geolocation.getCurrentPosition(showPosition);
 }
 
-export async function displayToPage(){
-    try{
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitudex}&lon=${longitudex}&appid=${apiKey}`,
-        {mode: 'cors'}
-        );
-        const data = await response.json()
-        console.log(data,"data")
-    }
-    catch(error){
-        console.log(error)
-    }
+export async function displayToPage() {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitudex}&lon=${longitudex}&appid=${apiKey}`,
+      { mode: 'cors' }
+    );
+    const data = await response.json();
+    console.log(data, 'data');
+  } catch (error) {
+    console.log(error);
+  }
 }
