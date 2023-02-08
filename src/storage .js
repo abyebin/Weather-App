2ab9d6a01fe42f0d7685b077619876aac8015d87
unknown <ebinaliyas5@gmail.com>
2023-02-08 18:45:31 +0530

    color change temp

--- src/storage .js
+++ src/storage .js
@@ -1,7 +1,10 @@
 import { toDisplay } from "./printOnDisplay"
+import { colorChange } from "./background"
 
 export let dataSet 
 
 export function manageData(){
+    colorChange()
     toDisplay()
+    
 }
